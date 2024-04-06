e5fa4b9b0cfef6a5e31f2d6541c6b02008f972f3
Derogater <thelazybloggers@gmail.com>
2024-04-06 18:05:26 +0530

    smooth3

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -16,7 +16,7 @@ export default function ImageGallery({images}: iAppProps) {
     }
     return (
         <div className="grid gap-4 lg: grid-cols-5">
-            <div className="order-last flex gap-4 lg:order-none lg:flex-col">
+            <div className="order-last grid gap-4 lg:order-none lg:flex-col">
                 {images.map((image: any, idx: any) => (
                     <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
                         <Image 
