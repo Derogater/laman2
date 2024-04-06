2645d4bcd91a5700c384986609eb0425f0cd228c
Derogater <thelazybloggers@gmail.com>
2024-04-06 17:48:49 +0530

    smooth

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,5 +1,32 @@
-export default function ProductPage() {
+import { fullProduct } from "@/app/interface";
+import { client } from "@/app/lib/sanity";
+import ImageGallery from "@/components/ImageGallery";
+
+async function getData(slug: string) {
+    const query = `*[_type == "product" && slug.current == "${slug}"][0]{
+        _id,
+        images,
+        price,
+        name,
+        description,
+        "slug": slug.current,
+        "categoryName": category->name,
+    }`;
+
+    const data = await client.fetch(query);
+
+    return data;
+}
+
+export default async function ProductPage({params}:{params: {slug: string};}) {
+    const data: fullProduct = await getData(params.slug);
     return (
-        <h1> Hello from product Page</h1>
+        <div className="bg-white">
+            <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+                <div className="grid gap-8 md:grid-cols-2">
+                    <ImageGallery images={data.images} />
+                </div>
+            </div>
+        </div>
     )
 }
--- components/Newest.tsx
+++ components/Newest.tsx
@@ -48,7 +48,7 @@ export default async function Newest() {
                             <div className="mt-4 flex justify-between">
                                 <div>
                                     <h3 className="text-sm text-gray-700">
-                                        <Link href={'/product/${product.slug}'}>
+                                        <Link href={`/product/${product.slug}`}>
                                         {product.name}
                                         </Link>
                                     </h3>
